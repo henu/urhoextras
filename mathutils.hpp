d75d85182c11d89479e75d3ab5db975e2e78923b
Henrik Heino <henrik.heino@gmail.com>
2018-01-23 20:38:19 +0200

    rotateVector2()

--- mathutils.hpp
+++ mathutils.hpp
@@ -2,6 +2,7 @@
 #define URHOEXTRAS_MATHUTILS_HPP
 
 #include <Urho3D/Math/Vector2.h>
+#include <Urho3D/Math/Matrix2.h>
 
 namespace UrhoExtras
 {
@@ -10,10 +11,22 @@ namespace UrhoExtras
 // distance is negative. Note, that distance is measured in length of
 // plane_normal, so if you want it to be measured in basic units, then
 // normalize plane_normal.
+inline float distanceTo2DPlane(Urho3D::Vector2 const& point, Urho3D::Vector2 const& plane_pos, Urho3D::Vector2 const& plane_normal);
+
+// Rotates vector clockwise
+inline Urho3D::Vector2 rotateVector2(Urho3D::Vector2 const& v, float angle);
+
 inline float distanceTo2DPlane(Urho3D::Vector2 const& point, Urho3D::Vector2 const& plane_pos, Urho3D::Vector2 const& plane_normal)
 {
 	float dp_nn = plane_normal.DotProduct(plane_normal);
 	return (plane_normal.DotProduct(point) - plane_normal.DotProduct(plane_pos)) / dp_nn;
+}
+
+inline Urho3D::Vector2 rotateVector2(Urho3D::Vector2 const& v, float angle)
+{
+	float s = Urho3D::Sin(angle);
+	float c = Urho3D::Cos(angle);
+	return Urho3D::Matrix2(c, s, -s, c) * v;
 }
 
 }
